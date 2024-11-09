61cc6f94d5f2baf1844228abe5ab2de9867e25e7
liamg2810 <lgardn3r@gmail.com>
2024-11-09 12:30:07 +0000

    syling for home page and personality quiz

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main>
+    <h1>Welcome to the Personality Diagnostic Quiz for Individual Learner Experiences</h1>
+    <button on:click={() => {window.location.href = "/personality"}}>Start</button>
+</main>
+
+<style>
+    main {
+        text-align: center;
+        color: #dddddd;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        height: 75vh;
+    }
+
+    h1 {
+        font-size: 2rem;
+        margin-bottom: 1rem;
+    }
+
+    button {
+        padding: 0.5rem 1rem;
+        background-color: var(--secondary);
+        color: #fff;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        transition: all 0.3s;
+        font-size: 1rem;
+    }
+
+    button:hover {
+        scale: 1.05;
+    }
+</style>
---
+++ app/src/routes/personality/+page.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+
+</script>
+
+<main>
+    <div class="top-bar">
+        <h2>Question 1 of 10</h2>
+
+    </div>
+    <div class="img-box"></div>
+    <div class="btns-container">
+        <button class="btn btn-not">Not me at all</button>
+        <button class="btn btn-notreally">Not really me</button>
+        <button class="btn btn-kinda">Kinda me</button>
+        <button class="btn btn-totally">Mostly me</button>
+        <button class="btn btn-mostly">Totally me</button>
+    </div>
+</main>
+
+<style>
+    main {
+        display: grid;
+        place-items: center;
+        grid-template-rows: 0.2fr 1fr 0.2fr;
+        height: 100vh;
+        padding: 1rem;
+    }
+
+    .img-box {
+        width: 100%;
+        height: 100%;
+        background-color: var(--secondary);
+        border-radius: 20px;
+    }
+
+    .btns-container {
+        display: flex;
+        gap: 1rem;
+        padding: 10px;
+        background-color: var(--secondary);
+        border-radius: 12px;
+    }
+
+    .btn {
+        padding: 0.5rem 1rem;
+        background-color: var(--secondary);
+        color: #222;
+        font-weight: 800;
+        border: none;
+        border-radius: 10px;
+        cursor: pointer;
+        transition: all 0.3s;
+        font-size: 0.92rem;
+    }
+
+    .btn:hover {
+        scale: 1.05;
+    }
+
+    .btn-not {
+        background-color: var(--notcolor);
+    }
+
+    .btn-notreally {
+        background-color: var(--notreallycolor);
+    }
+
+    .btn-kinda {
+        background-color: var(--kindacolor);
+    }
+
+    .btn-totally {
+        background-color: var(--mostlycolor);
+    }
+
+    .btn-mostly {
+        background-color: var(--totallycolor);
+    }
+
+    .top-bar h2 {
+        font-size: 1.5rem;
+        color: #ddd;
+    }
+
+    
+</style>
